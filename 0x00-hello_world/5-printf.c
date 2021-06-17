caf29bba13519e5f83fe098116ae286606f8bed0
hermella <hermellabirhanu056@gmail.com>
2021-06-17 12:23:24 -0700

    printf2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-/**                                                                                                  
- * main - Entry point                                                                                
- *                                                                                                   
- * Return: Always 0 (Success)                                                                        
- */             
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */        
 #include <stdio.h>
-int main (void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
