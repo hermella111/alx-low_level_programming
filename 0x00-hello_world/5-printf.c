c61135dc7abdf3e0cba4ae189c72be073315627d
hermella <hermellabirhanu056@gmail.com>
2021-06-17 12:27:10 -0700

    printf3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
- */        
+* main - Entry point
+* 
+* Return: Always 0 (Success)
+*/        
 #include <stdio.h>
 int main(void)
 {
