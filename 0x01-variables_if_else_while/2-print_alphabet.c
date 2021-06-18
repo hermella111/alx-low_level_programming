fa1a18480eb060e37c8b4fd6ff4df00a83163124
hermella <hermellabirhanu056@gmail.com>
2021-06-18 04:31:24 -0700

    hotfix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,20 +3,18 @@
 #include <stdio.h>                                                              
 /* more headers goes there */                                                   
 /**                                                           
+ * main - Entry point
  *
- * main - Entry point                                                            *                     
  * Return: Always 0 (Success)                                             
  */                                                                                                 
-
 /* betty style doc for function main goes there */                              
 int main(void)                                                          
 {
 char ch;
-for(ch = 'a'; ch <= 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
 putchar('\n');
-
-return(0);
+return (0);
 }
