b65b4e28e3d448d1ef52c9e8b1c9bc8fae6ffef4
hermella <hermellabirhanu056@gmail.com>
2021-06-18 13:03:58 -0700

    final

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,14 @@
-#include <stdio.h>                                                                                   
-/**                                                           
+#include <stdio.h>
+/**
  * main - Entry point
  *
- * Return: Always 0 (Success)                                             
- */                                                                                                   int main(void)                                                          
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-putchar(ch);
-}
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
 putchar('\n');
 return (0);
 }
