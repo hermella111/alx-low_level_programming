8bc4ea5314cfb5730f4897555e72c90a6e999e9e
hermella <hermellabirhanu056@gmail.com>
2021-06-18 12:43:59 -0700

    added

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,22 +6,31 @@
  */
 int main(void)
 {
-int c, i, k;
+int c, i, k, j;
 for (c = '0'; c <= '9'; c++)
 {
 for (i = '0'; i <= '9'; i++)
 {
 for (k = '0'; k <= '9'; k++)
 {
-if (c < i && i < k)
+for (j = 48; j<=57; j++)
+{
+if (((k + j) > (c + i) && k >= c) || c <k)
 {
 putchar(c);
 putchar(i);
+putchar(' ');
 putchar(k);
-if (c != '7')
+putchar(j);
+if (c + i + k + j == 227 && c == 57)
+{
+break;
+}
+else
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
