0d4b5921304a1e1f2eb126d70ff9c83732bf7908
hermella <hermellabirhanu056@gmail.com>
2021-06-18 05:27:30 -0700

    last fixx

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,10 @@ int main(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
-{
-if (ch == 'e'|| ch ==  'q')
+if (ch == 'e' || ch ==  'q')
 continue;
 else
 putchar(ch);
-}
 putchar('\n');
 return (0);
 } 
