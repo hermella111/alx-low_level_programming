7ce80a14136592f620e45f52e8fb8559c46a5904
hermella <hermellabirhanu056@gmail.com>
2021-06-22 13:59:07 -0700

    ff

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - sumeven fibonacci numbers under 4 million.
+ * Return: Nothing.
+ */
+ int main(void)
+{
+unsigned long count, i, j, k, sums;
+i  = sums =0;
+j = 1;
+:w
+:q
+
+:q
+:q
+
+
+
