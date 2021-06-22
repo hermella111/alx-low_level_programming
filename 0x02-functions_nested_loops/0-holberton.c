85a20490e7a29f6b9e4d2cc379c204c7b7e2dd77
hermella <hermellabirhanu056@gmail.com>
2021-06-22 05:44:23 -0700

    hot fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,25 +1,11 @@
 #include "holberton.h"
+/**
+ * main - Entry point for writing entire lowercase alphabet 
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char H, o, l, b, e, r, t, z, n;
-H = 'H';
-o = 'o';
-l = 'l';
-b = 'b';
-e = 'e';
-r = 'r';
-t = 't';
-z = 'o';
-n = 'n';
-putchar(H);
-putchar(o); 
-putchar(l); 
-putchar(b); 
-putchar(e); 
-putchar(r); 
-putchar(t); 
-putchar(z); 
-putchar(n);
-putchar('\n');
+_putchar('Holberton');
 return (0);
 }
