e55672e254f816a2a4cf1d1a174b168ccf31a395
hermella <hermellabirhanu056@gmail.com>
2021-06-22 11:03:34 -0700

    bulk commit

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,2 +1,14 @@
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
+void fibonaccia(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int jack_bauer(void);
+int print_last_digit(int);
+void print_times_table(int n);
+void times_table(void);
+void add(int a, int b);
+void print_to_98(int n);
