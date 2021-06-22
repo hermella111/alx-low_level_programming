54b70077470718eef46ddb8badb9db7ba6b71330
hermella <hermellabirhanu056@gmail.com>
2021-06-22 03:52:08 -0700

    0th project

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+int main(void)
+{
+char H, o, l, b, e, r, t, z, n;
+H = 'H';
+o = 'o';
+l = 'l';
+b = 'b';
+e = 'e';
+r = 'r';
+t = 't';
+z = 'o';
+n = 'n';
+putchar(H);
+putchar(o); 
+putchar(l); 
+putchar(b); 
+putchar(e); 
+putchar(r); 
+putchar(t); 
+putchar(o); 
+putchar(n);
+putchar('\n');
+return (0);
+}
