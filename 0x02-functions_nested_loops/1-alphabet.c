e9c7135bdaea1a02667823427c4731922ac4f9ae
hermella <hermellabirhanu056@gmail.com>
2021-06-22 10:37:49 -0700

    rt

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**                                                                             
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
