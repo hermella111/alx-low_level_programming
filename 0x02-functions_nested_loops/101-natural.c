f33904ea77f41b1fc20e1990a525fc7beaf2852c
hermella <hermellabirhanu056@gmail.com>
2021-06-23 09:15:30 -0700

    lastt

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - computes and prints the sum of all the multiples of 
- * 3 or 5 below 1024
+ * main - computes and prints the sum of all the multiples of 3 or 5 below 1024
  * Return: Nothing.
  */
 int main(void)
