de27b20aba4faed276d3282c9105b0c8dfd712cc
hermella <hermellabirhanu056@gmail.com>
2021-06-24 06:02:57 -0700

    9th

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0)
+printf("Fizz ");
+if (i % 5 == 0)
+printf("Buzz ");
+if (i % 5 == 0 && i % 3 == 0)
+printf("FizzBuzz ");
+else
+printf("%d ", i);
+}
+printf("\n");
+}
+	
