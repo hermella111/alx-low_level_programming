25e55b681eb049cf1c653ca23805c254eb384825
hermella <hermellabirhanu056@gmail.com>
2021-06-24 06:45:48 -0700

    debugging

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,11 +10,11 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0)
-printf("Fizz ");
+printf("%s", "Fizz ");
 if (i % 5 == 0)
-printf("Buzz ");
+printf("%s", "Buzz ");
 if (i % 5 == 0 && i % 3 == 0)
-printf("FizzBuzz ");
+printf("%s", "FizzBuzz ");
 else
 printf("%d ", i);
 }
